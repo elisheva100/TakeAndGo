471f3f7d6fb13c3b3fc442d48a6b567ea103908c
TairCohen <tairtif@gmail.com>
2018-01-10 21:46:26 +0200

    login

---
+++ app/src/main/java/com/example/owner/takeandgouser/controller/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.owner.takeandgouser.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.owner.takeandgouser.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
