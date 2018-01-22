bf31533c38478c0f4e6ebc159c2730f6abfbbe88
Elisheva Heilbron <elisheva.h02@gmail.com>
2018-01-22 19:05:11 +0200

    mapView

--- app/src/main/java/com/example/owner/takeandgouser/controller/BranchesFragment.java
+++ app/src/main/java/com/example/owner/takeandgouser/controller/BranchesFragment.java
@@ -2,6 +2,7 @@ package com.example.owner.takeandgouser.controller;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -14,11 +15,13 @@ import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.owner.takeandgouser.R;
 import com.example.owner.takeandgouser.model.backEnd.DBManagerFactory;
+import com.example.owner.takeandgouser.model.entities.Adress;
 import com.example.owner.takeandgouser.model.entities.Branch;
 
 import java.util.ArrayList;
@@ -29,16 +32,24 @@ public class BranchesFragment extends Fragment {
 
     private ExpandableListView branchesExpandableList;
     final MyExpandableListAdapter myBaseExpandableListAdapter = new MyExpandableListAdapter();
-    private static  List<Branch> Branches = new ArrayList<>() ;
+    private static List<Branch> Branches = new ArrayList<>();
+
+    private void openMap(Adress address) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri location = Uri.parse("geo:0,0?q=" + address.toString());
+        intent.setData(location);
+        startActivity(intent);
+    }
 
     public BranchesFragment() { // Required empty public constructor
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_branches, container, false);
-        branchesExpandableList =  ((ExpandableListView) rootView.findViewById(R.id.branchesExpandableList));
-        try{ new MyAsyncTask().execute();}
-        catch(Exception e) {
+        branchesExpandableList = ((ExpandableListView) rootView.findViewById(R.id.branchesExpandableList));
+        try {
+            new MyAsyncTask().execute();
+        } catch (Exception e) {
             Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
         }
         return rootView;
@@ -62,7 +73,7 @@ public class BranchesFragment extends Fragment {
         }
     }
 
-    class MyExpandableListAdapter extends BaseExpandableListAdapter{
+    class MyExpandableListAdapter extends BaseExpandableListAdapter implements View.OnClickListener {
         @Override
         public int getGroupCount() {
             return Branches.size();
@@ -102,8 +113,14 @@ public class BranchesFragment extends Fragment {
         public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
             View header = getActivity().getLayoutInflater().inflate(R.layout.branch_list_group, parent, false);
             TextView title = (TextView) header.findViewById(R.id.lblBranchesListHeader);
-            title.setText(Branches.get(groupPosition).getAdress().toString());
+            title.setText("Address: " + Branches.get(groupPosition).getAdress().toString());
+            ImageButton mapView = (ImageButton) header.findViewById(R.id.buttonMap);
+            mapView.setTag(Branches.get(groupPosition).getAdress());
+            mapView.setFocusable(false);
+            mapView.setOnClickListener(this);
+
             return header;
+
         }
 
         @Override
@@ -112,8 +129,8 @@ public class BranchesFragment extends Fragment {
             final Branch branch = Branches.get(groupPosition);
             TextView parking = (TextView) branchesListItem.findViewById(R.id.lblListParking);
             TextView branchNumber = (TextView) branchesListItem.findViewById(R.id.lblListBranchNumber);
-            parking.setText(String.valueOf(branch.getParking()));
-            branchNumber.setText(String.valueOf(branch.getBranchNumber()));
+            parking.setText("parking: " + String.valueOf(branch.getParking()));
+            branchNumber.setText("branch number: " + String.valueOf(branch.getBranchNumber()));
             return branchesListItem;
         }
 
@@ -122,6 +139,15 @@ public class BranchesFragment extends Fragment {
             return true;
         }
 
+        @Override
+        public void onClick(View v) {
+            if (v.getTag().getClass().equals(Adress.class)) {
+
+                Adress address = (Adress) v.getTag();
+
+                openMap(address);
+            }
+        }
     }
 }
 
